aa9514e5f51d7fbe41292298ace803db12e2def3
MrLuffyChan <imluffychan@gmail.com>
2024-12-26 20:59:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ serena_id = 2030938170
 api_id = os.getenv('API_ID', None)
 api_hash = os.getenv('API_HASH', None)
 session = os.getenv('SESSION', None)
-password = os.getenv('password', None)
-username = os.getenv('username', None)
+password = os.getenv('password', 'bala@2005')
+username = os.getenv('username', 'Serena_AI')
 db_url = os.getenv('DB_URL', "mongodb+srv://kora3244:jNtUZleBWM71f1pC@haremdb.qxtdvdh.mongodb.net/?retryWrites=true&w=majority")
 chatbot_url = os.getenv('CHATBOT', None)
 char_id = "UkuPaK3" #this is serena is character id [ https://www.sakura.fm]
