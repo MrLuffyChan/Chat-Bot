fa4db99d7f72d0f087b54dc22a77eee2a1fbe044
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-12-26 18:24:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ api_hash = os.getenv('API_HASH', None)
 session = os.getenv('SESSION', None)
 password = os.getenv('password', None)
 username = os.getenv('username', None)
-db_url = os.getenv('DB_URL', None)
+db_url = os.getenv('DB_URL', "mongodb+srv://kora3244:jNtUZleBWM71f1pC@haremdb.qxtdvdh.mongodb.net/?retryWrites=true&w=majority")
 chatbot_url = os.getenv('CHATBOT', None)
 char_id = "UkuPaK3" #this is serena is character id [ https://www.sakura.fm]
