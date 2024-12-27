2d34a60f8d3f9261e330b3c1cad9dffe65f72543
MrLuffyChan <imluffychan@gmail.com>
2024-12-27 20:52:34 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -26,8 +26,6 @@ def web_server():
 
 async def keep_alive():
     if WEB_URL:
-        while True:
-            await asyncio.sleep(WEB_SLLEP)
             try:
                 async with aiohttp.ClientSession(
                     timeout=aiohttp.ClientTimeout(total=10)
