e6d25807e2c5df8bff9cc81b4ea9572766095a9b
MrLuffyChan <imluffychan@gmail.com>
2024-12-27 21:08:56 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -33,7 +33,7 @@ async def keep_alive():
                                 WEB_URL, resp.status
                             )
                         )
-                )
+                    )
             except asyncio.TimeoutError:
                 log.warning("Couldn't connect to the site URL..!")
             except Exception:
