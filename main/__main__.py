3675b0d03ea9380af3b11e82c7e66e08be29c746
MrLuffyChan <imluffychan@gmail.com>
2024-12-27 11:21:56 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -1,9 +1,48 @@
-import pyrogram
-import asyncio
-
-from web import keep_alive, web_server
 from aiohttp import web
 from serena import serena
+
+import pyrogram, asyncio, aiohttp, logging, traceback
+
+WEB_URL = "https://chat-bot-o8pm.onrender.com"
+WEB_SLLEP = 3*60
+
+log = logging.getLogger(__name__)
+
+
+routes = web.RouteTableDef()
+
+@routes.get('/', allow_head=True)
+async def hello(request):
+    return web.Response(text="Hello, world!")
+
+
+def web_server():
+    app = web.Application()
+    app.add_routes(routes)
+    return app
+
+
+
+
+async def keep_alive():
+    if WEB_URL:
+        while True:
+            await asyncio.sleep(WEB_SLLEP)
+            try:
+                async with aiohttp.ClientSession(
+                    timeout=aiohttp.ClientTimeout(total=10)
+                ) as session:
+                    async with session.get(WEB_URL) as resp:
+                        log.info(
+                            "Pinged {} with response: {}".format(
+                                WEB_URL, resp.status
+                            )
+                        )
+            except asyncio.TimeoutError:
+                log.warning("Couldn't connect to the site URL..!")
+            except Exception:
+                traceback.print_exc()
+                    
 
 BIND_ADDRESS = "0.0.0.0"
 PORT = "8080"
